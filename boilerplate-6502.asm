0f583751c87f6230ed34d38efbd1ebc9669ec5d2
Guy Matz <gmatz@jetblue.com>
2022-09-20 16:49:15 -0400

    boilerplate code for all atari-2600 programs

---
+++ boilerplate-6502.asm
@@ -0,0 +1,10 @@
+  processor 6502
+  seg Code        ; Define a new segment named Code
+  org $F000       ; Define the origin of the ROM Code at address $F000
+Start:
+
+
+
+  org $FFFC       ; End the ROM by adding required values to memory
+  .word Start     ; Put 2 bytes with the reset address
+  .word Start     ; Put 2 bytes with the break address
