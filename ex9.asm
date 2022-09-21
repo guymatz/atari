2af13b5b02041c2bdc946080ac27e5796b2b9bc0
Guy Matz <gmatz@jetblue.com>
2022-09-21 15:26:13 -0400

    done

--- ex9.asm
+++ ex9.asm
@@ -2,6 +2,15 @@
   seg Code        ; Define a new segment named Code
   org $F000       ; Define the origin of the ROM Code at address $F000
 Start:
+   lda #1 ; Initialize the A register with the decimal value 1
+   clc
+Loop:
+  ; Increment A
+  
+  adc #1
+  ; Compare the value in A with the decimal value 10
+  cmp #10
+  bne Loop; Branch back to loop if the comparison was not equals (to zero)
 
 
 
